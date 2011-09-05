10611d7c9dcc812b52d4b10a66ae844a37d6773e
bongole <bongole2@gmail.com>
2011-09-05 18:18:37 +0900

    Support composite primary keys serialization.

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -2,7 +2,15 @@ class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
   def self.yaml_new(klass, tag, val)
-    klass.find(val['attributes']['id'])
+    pks = klass.primary_key
+    case pks
+    when Array
+        ids = pks.map{|pk| val['attributes'][pk.to_s] }
+    else
+        ids = val['attributes']['id']
+    end
+
+    klass.find(ids)
   rescue ActiveRecord::RecordNotFound
     raise Delayed::DeserializationError
   end
